12c20e1ddb20a46519a682d006843813b2783ef2
DamirF <d.fatkhullin@mail.ru>
2022-06-16 23:56:52 +0300

    GameController started

--- Durak_BL/Controller/GameController.cs
+++ Durak_BL/Controller/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Durak_BL.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Durak_BL.Controller
 {
     public class GameController
     {
+        PackController packController;
+        List<Player> Players; // maybe will be changed to PlayerController;
+
+        public GameController()
+        {
+            packController = new PackController(ref Players);
+        }
     }
 }
